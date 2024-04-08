6daf254d42d53de80786d012c1fa0ff3c419d119
Hieu Duong <hieuduong612001@berkeley.edu>
2024-04-08 02:01:58 -0700

    DONE

--- tests/integration-custom/in/test.s
+++ tests/integration-custom/in/test.s
@@ -1,6 +1,8 @@
-        addi s0, x0, 1  # 1, addr 0x00
-        addi s1, x0, 2  # 2,  addr 0x04
         jal  x0, label1 # 3,  addr 0x08
-        addi s1, s0, -1 # x,  addr 0x0C
+        addi s1, s0, 5 # x,  addr 0x0C
         addi s1, s0, -2 # x,  addr 0x10
 label1: addi s1, x0, 0  # 4,  addr 0x14
+        jal  x0, label2 # 3,  addr 0x08
+        addi s1, s0, 7 # x,  addr 0x0C
+        addi s1, s0, -2 # x,  addr 0x10
+label2: addi s1, x0, 0  # 4,  addr 0x14
